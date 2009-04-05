54cfa36e906692511fc7c44537f0aa28ecd7c2e1
mturner <mturner@SUNNY.(none)>
2009-04-04 21:29:54 -0500

    4 complete

--- src/004.py
+++ src/004.py
@@ -2,4 +2,31 @@
 http://www.pythonchallenge.com/pc/def/linkedlist.html
 http://www.pythonchallenge.com/pc/def/linkedlist.php
 '''
+import urllib
 
+baseurl = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing="
+nothing = "33179"
+nothings = []
+text = ""
+repeat_count = 0
+for i in range(400):
+    handle = urllib.urlopen(baseurl+nothing)
+    text = handle.read()
+    print i, ":", nothing, ":", text
+    if nothing in nothings:
+        print "repeat"
+        repeat_count += 1
+        if repeat_count >10: break
+    else:
+        nothings.append(nothing)
+    if " ".join(text.split()[:-1]) != "and the next nothing is":
+        print text
+    if text == "Yes. Divide by two and keep going.":
+        print nothing
+        nothing = str(int(nothing)/2)
+        print nothing
+    else:
+        nothing = text.split()[-1]
+        if not nothing.isdigit(): break
+
+print text
